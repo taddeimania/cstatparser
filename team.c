51f5c5294e3546c8f8bfc13e44520e93fb873f87
taddeimania <jtaddei@gmail.com>
2015-10-07 14:08:33 -0400

    small refactor on team name

--- team.c
+++ team.c
@@ -1,23 +1,22 @@
 #include "team.h"
 
+
+struct Team get_team(unsigned char buffer[10000], int offset)
+{
+    struct Team team;
+
+    for(int i = 0; i <= 2 ; i++) {
+        team.name[i] = buffer[3087 + offset + i];
+    }
+    return team;
+}
+
 struct Team get_home_team(unsigned char buffer[10000])
 {
-    struct Team home_team;
-    char home[2];
-
-    for(int i = 0; i <= sizeof(home) ; i++) {
-        home_team.name[i] = buffer[3087 + i];
-    }
-    return home_team;
+    return get_team(buffer, 0);
 }
 
 struct Team get_away_team(unsigned char buffer[10000])
 {
-    struct Team away_team;
-    char away[2];
-
-    for(int i = 0; i <= sizeof(away) ; i++) {
-        away_team.name[i] = buffer[3087 + 32 + i];
-    }
-    return away_team;
+    return get_team(buffer, 32);
 }
--- team.h
+++ team.h
@@ -3,5 +3,6 @@ struct Team {
     char name[4];
 };
 
+struct Team get_team(unsigned char buffer[10000], int offset);
 struct Team get_home_team(unsigned char buffer[10000]);
 struct Team get_away_team(unsigned char buffer[10000]);
