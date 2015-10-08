4867d1cd028a8800e9efd7edc1fff5ab158191d0
taddeimania <jtaddei@gmail.com>
2015-10-07 22:30:10 -0400

    more cleanup

--- player.c
+++ player.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include "player.h"
 
-void add_stats(unsigned char *buffer, Player *player, int offset){
+void add_stats(unsigned char *buffer, Player *player, int offset)
+{
     if (strcmp(player->pos, "QB1") == 0) {
         player->pass_attempts = buffer[5781 + offset];
     } else {
