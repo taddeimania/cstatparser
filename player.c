ab2db72efcbeb2673c1317b73decf0281d3cc5e3
taddeimania <jtaddei@gmail.com>
2015-10-08 15:09:17 -0400

    check for QB1 or QB2 position

--- player.c
+++ player.c
@@ -12,9 +12,11 @@ Player *new_player()
 
 void add_stats(unsigned char *buffer, Player *player, int offset)
 {
-    if (strcmp(player->pos, "QB1") == 0) {
+    if (strcmp(player->pos, "QB1") == 0 || strcmp(player->pos, "QB2")) {
         player->pass_attempts = buffer[5781 + offset];
+        player->pass_completions = buffer[5781 + 1 + offset];
     } else {
         player->pass_attempts = 0;
+        player->pass_completions = 0;
     }
 }
